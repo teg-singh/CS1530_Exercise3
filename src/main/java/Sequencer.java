7815d9c076eab7a25d780433f2e8605cda287404
teg-singh <tes70@pitt.edu>
2017-10-25 13:31:29 -0400

    Fixed Triangle

--- src/main/java/Sequencer.java
+++ src/main/java/Sequencer.java
@@ -19,8 +19,8 @@ public class Sequencer {
 	}
 	
 	public static int triangle(int val) {
-		if (val = 1) {
-			return 1;
+		if (val = 0) {
+			return 0;
 		}
 		else {
 			return val + triangle(val - 1);
